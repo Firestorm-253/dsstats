e49900994511aa3d18a2f82ccf773d1293c84a79
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-01-19 00:49:46 +0100

    ReplayService

--- src/dsstats.mmr.cli/Program.cs
+++ src/dsstats.mmr.cli/Program.cs
@@ -8,34 +8,7 @@ class Program
 {
     static void Main(string[] args)
     {
-    }
 
 
-    static List<ReplayRatingDto> ProduceRatings(List<ReplayDsRDto> replays, MmrOptions mmrOptions)
-    {
-        var replayRatings = new List<ReplayRatingDto>();
-        var mmrIdRatings = new Dictionary<int, CalcRating>();
-        var cmdrMmrDic = new Dictionary<CmdrMmmrKey, CmdrMmmrValue>();
-
-        foreach (var replay in replays)
-        {
-            var (replayRatingDto, _) = MmrService.ProcessReplay(replay, mmrIdRatings, cmdrMmrDic, mmrOptions);
-            replayRatings.Add(replayRatingDto!);
-        }
-
-        return replayRatings;
-    }
-
-    static List<ReplayDsRDto> GetReplayDsRDtos()
-    {
-        var json = File.ReadAllBytes("database.json");
-        var jsonReplays = JsonSerializer.Deserialize<List<ReplayDsRDto>>(json);
-
-        if (jsonReplays == null)
-        {
-            throw new Exception();
-        }
-
-        return jsonReplays;
     }
 }
---
+++ src/dsstats.mmr.cli/ReplayService.cs
@@ -0,0 +1,35 @@
+﻿using pax.dsstats.shared;
+using System.Text.Json;
+
+namespace dsstats.mmr.cli;
+
+public static class ReplayService
+{
+    public static List<ReplayRatingDto> ProduceRatings(List<ReplayDsRDto> replays, MmrOptions mmrOptions)
+    {
+        var replayRatings = new List<ReplayRatingDto>();
+        var mmrIdRatings = new Dictionary<int, CalcRating>();
+        var cmdrMmrDic = new Dictionary<CmdrMmmrKey, CmdrMmmrValue>();
+
+        foreach (var replay in replays)
+        {
+            var (replayRatingDto, _) = MmrService.ProcessReplay(replay, mmrIdRatings, cmdrMmrDic, mmrOptions);
+            replayRatings.Add(replayRatingDto!);
+        }
+
+        return replayRatings;
+    }
+
+    public static List<ReplayDsRDto> GetReplayDsRDtos()
+    {
+        var json = File.ReadAllBytes("database.json");
+        var jsonReplays = JsonSerializer.Deserialize<List<ReplayDsRDto>>(json);
+
+        if (jsonReplays == null)
+        {
+            throw new Exception();
+        }
+
+        return jsonReplays;
+    }
+}
