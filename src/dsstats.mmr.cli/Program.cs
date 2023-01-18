d19942e1734caa0a3ba134d162d52bd7410bcf67
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-01-19 00:29:42 +0100

    ProduceRatings()

--- src/dsstats.mmr.cli/Program.cs
+++ src/dsstats.mmr.cli/Program.cs
@@ -8,6 +8,22 @@ class Program
     {
     }
 
+
+    static List<ReplayRatingDto> ProduceRatings(List<ReplayDsRDto> replays, MmrOptions mmrOptions)
+    {
+        var replayRatings = new List<ReplayRatingDto>();
+        var mmrIdRatings = new Dictionary<int, CalcRating>();
+        var cmdrMmrDic = new Dictionary<CmdrMmmrKey, CmdrMmmrValue>();
+
+        foreach (var replay in replays)
+        {
+            var (replayRatingDto, _) = MmrService.ProcessReplay(replay, mmrIdRatings, cmdrMmrDic, mmrOptions);
+            replayRatings.Add(replayRatingDto!);
+        }
+
+        return replayRatings;
+    }
+
     static List<ReplayDsRDto> GetReplayDsRDtos()
     {
         var json = File.ReadAllBytes("database.json");
