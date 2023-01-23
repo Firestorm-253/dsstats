a518e74fafa46ac867f6a0c2cdaa4aec3508c920
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-01-23 23:55:17 +0100

    RatingOverTime to csv-file

--- src/dsstats.mmr.cli/Program.cs
+++ src/dsstats.mmr.cli/Program.cs
@@ -1,4 +1,7 @@
 ﻿global using TeamData = dsstats.mmr.cli.ProcessData.TeamData;
+using System.Globalization;
+using System.Text;
+using CsvHelper;
 
 namespace dsstats.mmr.cli;
 
@@ -12,5 +15,77 @@ class Program
         //var result_ConfidenceTest = Tests.ConfidenceTest(replayRatings, 16333);
         
         var result_DerivationTest = Tests.DerivationTest(replays);
+    }
+
+    public static void WriteToCsv(string file, KeyValuePair<double, (int, double)[]>[] dic)
+    {
+        WriteToCsv_V2(file, dic);
+        return;
+
+
+        var csv = new StringBuilder();
+
+        var line1 = ",";
+        foreach (var ent in dic)
+        {
+            line1 += ent.Key + ",";
+        }
+        csv.AppendLine(line1.TrimEnd(','));
+
+        for (int i = 0; i < dic.Length; i++)
+        {
+            var ent = dic.ElementAt(i).Value;
+
+            for (int m = 0; m < ent.Length; m++)
+            {
+
+            }
+        }
+        
+        //foreach (var ent in dic)
+        //{
+        //    var line = string.Empty;
+        //    for (int i = 0; i < ent.Value.Length; i++)
+        //    {
+        //        line += ent.Value[i].ToString() + ",";
+        //    }
+        //    csv.AppendLine(line.TrimEnd(','));
+        //}
+
+        File.WriteAllText(file, csv.ToString());
+    }
+    
+    public static void WriteToCsv_V2(string file, KeyValuePair<double, (int, double)[]>[] dic)
+    {
+        using (var sw = new StreamWriter(file))
+        {
+            using (var csvWriter = new CsvWriter(sw, System.Globalization.CultureInfo.CurrentCulture))
+            {
+                var records = new List<object>();
+                for (int i = 0; i < dic.First().Value.Length; i++)
+                {
+                    var record = new { Games = dic.First().Value[i].Item1,
+                        Clip_1000 = dic[0].Value[i].Item2,
+                        Clip_1500 = dic[1].Value[i].Item2,
+                        Clip_2000 = dic[2].Value[i].Item2,
+                        Clip_2500 = dic[3].Value[i].Item2,
+                        Clip_3000 = dic[4].Value[i].Item2,
+                        Clip_3500 = dic[5].Value[i].Item2,
+                    };
+
+                    records.Add(record);
+                }
+                //var records = new List<object>
+                //{
+                //    new { Games = dic.ElementAt(0).Value[i].Item1, Clip_1600 = 0, Clip_2100 = 0 },
+                //    new { Games = 100, Clip_1600 = 0.5, Clip_2100 = 0.60  },
+                //    new { Games = 200, Clip_1600 = 0.63, Clip_2100 = 0.78 },
+                //};
+
+                csvWriter.WriteRecords(records);
+            }
+        }
+
+        File.WriteAllText(file, File.ReadAllText(file).Replace(';', ','));
     }
 }
--- src/dsstats.mmr.cli/Tests.cs
+++ src/dsstats.mmr.cli/Tests.cs
@@ -14,67 +14,125 @@ public static class Tests
     public static List<(double, double, double, double)> DerivationTest(List<ReplayDsRDto> replayDsRDtos)
     {
         const double startClip = 168;
-        double clip = 1600; //startClip;
-        double realAccuracy;
-        double loss;
+        double clip = 20000; //startClip;
+        double realAccuracy = 0;
+        double loss = double.MaxValue;
         // double loss_d;
+
+
+        var dic = new Dictionary<double, (int, double)[]>();
 
         var results = new List<(double, double, double, double)>();
         do
         {
-            var replayRatings = ReplayService.ProduceRatings(replayDsRDtos, new MmrOptions(true, startClip, clip));
+            //replayDsRDtos = replayDsRDtos.Where(x => x.ReplayPlayers.Any(x => x.Player.Name == "Kragh")).ToList();
+            var replayDatas = ReplayService.ProduceRatings(replayDsRDtos, new MmrOptions(true, startClip, clip));
 
-            realAccuracy = replayRatings.Count(x => x.CorrectPrediction) / (double)replayRatings.Count;
 
-            loss = Loss.GetLoss(clip, replayRatings);
-            //loss_d = Loss.GetLoss_d(clip, replayRatings.ToArray());
+            //var aot = GetAccuracyOverTime(GetAccuracyOverTime(replayDatas));
+            var aoga = GetAccuracyOverGamesAmount(replayDatas);
 
-            results.Add((clip, realAccuracy, loss, 0/*-loss_d*/));
+            //dic.Add(clip, aoga.Select(x => (x.Key, x.Value)).ToArray());
 
-            //clip += 500;//-loss_d * startClip;
+
+            //if (dic.Count == 6)
+            //{
+            //    Program.WriteToCsv_V2("table.csv", dic.ToArray());
+            //}
+
+            //realAccuracy = replayDatas.Count(x => x.CorrectPrediction) / (double)replayDatas.Count;
+
+            //loss = Loss.GetLoss(clip, replayRatings);
+            ////loss_d = Loss.GetLoss_d(clip, replayRatings.ToArray());
+
+            //results.Add((clip, realAccuracy, loss, 0/*-loss_d*/));
+
+            clip += 500;//-loss_d * startClip;
         } while (loss > 0);
+
+        Program.WriteToCsv("table.csv", dic.ToArray());
 
         return results;
     }
 
-    public static double ConfidenceTest(List<ReplayRatingDto> replayRatings, int playerId = 10758)
+    public static Dictionary<int, double> GetAccuracyOverGamesAmount(List<ReplayData> replayDatas)
     {
-        replayRatings = GetReplayRaitingsOfPlayer(playerId, replayRatings);
+        var accuracyOverGamesAmount = new Dictionary<int, double>();
+        const int range = 1000;
 
-        var winRate = GetWinRate(playerId, replayRatings);
-        var avgETW = GetAvgETW(playerId, replayRatings);
-        var confidence = 1 - Math.Abs(winRate - avgETW);
-
-        return confidence;
-    }
-
-    private static double GetAvgETW(int playerId, List<ReplayRatingDto> replayRatings)
-    {
-        double avgETW_sum = 0;
-        foreach (var replayRating in replayRatings)
+        for (int i = 0; i < replayDatas.Count; i++)
         {
-            if (replayRating.WinnerTeamData.Players.Any(p => p.PlayerId == playerId))
+            int key = ((i / range) * range) + range;
+            double gain = replayDatas[i].CorrectPrediction ? 1 : 0;
+
+            if (!accuracyOverGamesAmount.ContainsKey(key))
             {
-                avgETW_sum += replayRating.WinnerTeamData.ExpectedResult;
+                accuracyOverGamesAmount[key] = 0;
             }
-            else
-            {
-                avgETW_sum += replayRating.LoserTeamData.ExpectedResult;
-            }
+            accuracyOverGamesAmount[key] += gain;
         }
-        return avgETW_sum / replayRatings.Count;
+
+        for (int i = 0; i < accuracyOverGamesAmount.Count; i++)
+        {
+            int key = accuracyOverGamesAmount.ElementAt(i).Key;
+            accuracyOverGamesAmount[key] = Math.Round(accuracyOverGamesAmount[key] / range, 2);
+        }
+
+        if (replayDatas.Count % range != 0)
+        {
+            accuracyOverGamesAmount.Remove(accuracyOverGamesAmount.Keys.Last());
+        }
+        return accuracyOverGamesAmount;
     }
 
-    private static double GetWinRate(int playerId, List<ReplayRatingDto> replayRatings)
+    public static Dictionary<string, (double, int)> GetAccuracyOverTime(List<ReplayData> replayDatas)
     {
-        int winCounts = replayRatings.Count(r => r.WinnerTeamData.Players.Any(p => p.PlayerId == playerId));
-        return winCounts / (double)replayRatings.Count;
+        var amountOverTime = new Dictionary<DateTime, int>();
+        var correctOverTime = new Dictionary<DateTime, int>();
+
+        foreach (var replayData in replayDatas)
+        {
+            int year = replayData.GameTime.Year;
+            int month = replayData.GameTime.Month;
+
+            var key = new DateTime(year, month, 1);
+            if (!amountOverTime.ContainsKey(key))
+            {
+                amountOverTime[key] = 0;
+            }
+            if (!correctOverTime.ContainsKey(key))
+            {
+                correctOverTime[key] = 0;
+            }
+
+            amountOverTime[key]++;
+            correctOverTime[key] += replayData.CorrectPrediction ? 1 : 0;
+        }
+
+        var accuracyOverTime = new Dictionary<string, (double, int)>();
+
+        foreach (var ent in amountOverTime)
+        {
+            string strgKey = ent.Key.ToString("MMMM/yyyy");
+            accuracyOverTime[strgKey] = (Math.Round(correctOverTime[ent.Key] / (double)amountOverTime[ent.Key], 3), amountOverTime[ent.Key]);
+        }
+
+        return accuracyOverTime;
     }
 
-    private static List<ReplayRatingDto> GetReplayRaitingsOfPlayer(int playerId, List<ReplayRatingDto> replayRatings)
+    private static List<string> GetAccuracyOverTime(Dictionary<string, (double, int)> dic)
     {
-        var listWinnerTeam = replayRatings.Where(r => r.WinnerTeamData.Players.Any(p => p.PlayerId == playerId));
-        var listLoserTeam = replayRatings.Where(r => r.LoserTeamData.Players.Any(p => p.PlayerId == playerId));
-        return listWinnerTeam.Concat(listLoserTeam).ToList();
+        var lines = new List<string>();
+        foreach (var ent in dic)
+        {
+            //lines.Add(string.Format("Accuracy: {1}% | Games: {2} | Date: {0}", ent.Key, Math.Round(ent.Value.Item1 * 100), ent.Value.Item2));
+            lines.Add(string.Format("{2} Games ({1}%) - {0}", ent.Key, Math.Round(ent.Value.Item1 * 100), ent.Value.Item2));
+        }
+        return lines;
+    }
+
+    public static double ConfidenceTest(int playerId)
+    {
+        return double.NaN;
     }
 }
